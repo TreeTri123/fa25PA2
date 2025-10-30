c78db96d3b6ab2c6bd54524dab4cc7d0fabb7199
TreeTri123 <triannanguyentu@gmail.com>
2025-10-29 18:45:49 -0700

    Push and Uphead Logic before implementation

--- heap.h
+++ heap.h
@@ -7,6 +7,7 @@
 
 #include <iostream>
 using namespace std;
+extern int weightArr[];
 
 struct MinHeap {
     int data[64];
@@ -15,7 +16,12 @@ struct MinHeap {
     MinHeap() { size = 0; }
 
     void push(int idx, int weightArr[]) {
+        data[size] = weightArr[idx];
+        size++;
+        upheap(size - 1);
+        }
         // TODO: insert index at end of heap, restore order using upheap()
+
     }
 
     int pop(int weightArr[]) {
@@ -25,7 +31,27 @@ struct MinHeap {
     }
 
     void upheap(int pos, int weightArr[]) {
+    int left = pos*2 + 1;
+    int right = pos*2 + 2;
+    int root = (pos - 1) / 2;
+
+    if (root < weightArr[left] && weightArr[right]) {
+        //no change
+    }
+    if (root > weightArr[left]) {
+        swap(weightArr[root], weightArr[left]);
+    }
+    if (root > weightArr[right]) {
+        swap(weightArr[root], weightArr[right]);
+    }
+
+    //parent
+    (i - 1) / 2
+    //children
+    2*i + 1, 2*i +2;
+
         // TODO: swap child upward while smaller than parent
+
     }
 
     void downheap(int pos, int weightArr[]) {
